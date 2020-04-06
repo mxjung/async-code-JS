6e2642f3fbd0f4f71043ae6bb78cc3f03e1e371a
Charles Kantz <kantz.charles@gmail.com>
2020-04-06 14:38:39 -0700

    part 2 complete

--- Part2/script.js
+++ Part2/script.js
@@ -1,16 +1,26 @@
+let DECK_ID;
 
-console.log('hello');
-async function get_cards(){
-  
+async function grabDeck(){
+  // grabbing deck
   let shuffle_resp = await axios.get(`https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1`);
+  DECK_ID = shuffle_resp.data.deck_id;
 
-  let deck_id = shuffle_resp.data.deck_id;
-
-  let card_resp1 = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
-  let card_resp2 = await axios.get(`https://deckofcardsapi.com/api/deck/${deck_id}/draw/?count=1`);
-
-  // console.log(card_resp);
-  console.log(`${card_resp1.data.cards[0].value} of ${card_resp1.data.cards[0].suit}`);
-  console.log(`${card_resp2.data.cards[0].value} of ${card_resp2.data.cards[0].suit}`);
 }
 
+// async function get_card(){
+// }
+
+$("#gimme-card").on("click", async function(){
+  // let card = get_card();
+  
+  let card = await axios.get(`https://deckofcardsapi.com/api/deck/${DECK_ID}/draw/?count=1`);
+  let cardImage = card.data.cards[0].image;
+  let $cardArea = $('#cards-here');
+  let randomAngle = Math.round(Math.random() * 100);
+  console.log(randomAngle);
+
+  $cardArea.append(`<img src="${cardImage}" style="transform: rotate(${randomAngle}deg);">`);
+
+});
+
+grabDeck();
